78579cefab5bde9be5144b6200184043c4cf9dd9
kamilgaborek <kamil.gaborek@gmail.com>
2020-03-22 09:46:22 +0100

    implemented map based sercvices

--- pet-clinic-data/src/main/java/kamil/gaborek/petclinicspringboot/services/OwnerService.java
+++ pet-clinic-data/src/main/java/kamil/gaborek/petclinicspringboot/services/OwnerService.java
@@ -2,8 +2,6 @@ package kamil.gaborek.petclinicspringboot.services;
 
 import kamil.gaborek.petclinicspringboot.model.Owner;
 
-import java.util.Set;
-
 interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
